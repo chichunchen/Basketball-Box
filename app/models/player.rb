0f8a803f123e2eac60d2cff7e25b1602542d8263
George Chen <chi.chun_chen844@icloud.com>
2014-09-18 21:00:34 +0800

    player validation

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,6 @@
 class Player < ActiveRecord::Base
+	validates :number, :name, :presence => true	
+	validates :number, :inclusion => { :in => 0..99, :message => "請選擇號碼0至99號" }
+
 	belongs_to :team
 end
