80f4ce6c51d745b241743674c67fed701a938f76
chi chun chen <chi.chun_chen844@icloud.com>
2014-09-19 17:40:35 +0800

    player uniqueness

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,7 @@
 class Player < ActiveRecord::Base
 	validates :number, :name, :presence => true	
 	validates :number, :inclusion => { :in => 0..99, :message => "請選擇號碼0至99號" }
-
+	validates :number, :uniqueness => true
+	
 	belongs_to :team
 end
